74047a62b35ca94af285a1f728ce0670aa690b77
Josh Broitman <jbro132@gmail.com>
2020-03-26 14:45:28 -0400

    basic urls setup and dj_profile

--- webapps/urls.py
+++ webapps/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.autodiscover()
+
+from songshare import views
 
 urlpatterns = [
+    path('',views.login_action, name='login'),
     path('admin/', admin.site.urls),
+    path('songshare/', include('songshare.urls')),
 ]
