1208920365482ca32c11d4cc40d060b00cf6abb3
nathan koch <kochnathan73@gmail.com>
2020-04-09 23:43:17 -0400

    removed path for dj-stream as it has been replaced

--- songshare/urls.py
+++ songshare/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('login',views.login_action,name="login"),
     path('register', views.register_action, name="register"),
     path('logout',views.logout_action,name="logout"),
-    path('dj-stream', views.dj_stream, name="dj-stream"),
     path('listener-stream/<int:id>', views.listener_stream, name="listener-stream"),
     path('dj-search', views.dj_search, name="dj-search"),
     path('song-search/<int:id>', views.song_search, name="song-search"),
